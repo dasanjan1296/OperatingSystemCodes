aa185ea5ba0f5baaa2727eeed9142d7f383ee01a
dasanjan1296 <dasanjan1296@gmail.com>
2018-01-08 09:53:01 +0530

    Add svr.c

---
+++ svr.c
@@ -0,0 +1,41 @@
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<stdio.h>
+
+#define MSGKEY 75
+
+struct msgform
+{
+	long mtype;
+	char mtext[256];
+}msg;
+
+int msgid;
+
+main()
+{
+	int i,pid,*pint;
+	extern cleanup();
+
+	//for(i=0;i<20;i++)
+	//	signal(i,cleanup);
+	msgid = msgget(MSGKEY,0777|IPC_CREAT);
+
+	for(; ;)
+	{
+		msgrcv(msgid,&msg,256,1,0);
+		pint = (int*)msg.mtext;
+		pid = *pint;
+		printf("server:receive from pid %d\n",pid);
+		msg.mtype = pid;
+		*pint = getpid();
+		msgsnd(msgid, &msg,sizeof(int),0);
+	}
+}
+
+ceanup()
+{
+	msgctl(msgid,IPC_RMID,0);
+	//exit(0);
+}
